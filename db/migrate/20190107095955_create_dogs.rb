71c1a530c0943310e1060ee282e9851cc827c495
Irene <irene@irenemeisel.net>
2019-01-07 05:12:00 -0500

    here we go

---
+++ db/migrate/20190107095955_create_dogs.rb
@@ -0,0 +1,11 @@
+class CreateDogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dogs do |t|
+      t.string :breed
+      t.string :gender
+      t.integer :age
+
+      t.timestamps
+    end
+  end
+end
